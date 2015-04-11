ec532f0435cace56a299a3aaee5cf0fad64fce14
Ennima <ennima@hotmail.com>
2015-04-10 21:07:00 -0500

    mysql connect

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+   host: 'us-cdbr-azure-southcentral-e.cloudapp.net',
+   user: 'b9c638fb27f6cd',
+   password: '29c42091',
+   database: 'ennimaTestDB1',
+   port: 3306
+});
+connection.connect(function(error){
+   if(error){
+      throw error;
+   }else{
+      console.log('Conexion correcta.');
+   }
+});
+connection.end();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
